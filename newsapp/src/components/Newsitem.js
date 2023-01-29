2a52379c91d6a0358f6ea119340b6d5349d563d1
arnavsar <56104491+Arnav2300@users.noreply.github.com>
2023-01-30 01:25:32 +0530

    fetching from API successfully

--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -7,11 +7,23 @@ export class Newsitem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageURL} class="card-img-top" alt="..." />
+          <img
+            src={
+              !imageURL
+                ? "https://media.cnn.com/api/v1/images/stellar/prod/230118082443-01-brovary-helicopter-crash-011823.jpg?c=16x9&q=w_800,c_fill"
+                : imageURL
+            }
+            class="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsURL} target="_blank" className="btn btn-primary btn-sm">
+            <a
+              href={newsURL}
+              target="_blank"
+              className="btn btn-primary btn-sm"
+            >
               Read more ...
             </a>
           </div>
